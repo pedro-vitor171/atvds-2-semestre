f830cd0d056e2a8b41ce947368ac00b1f420882a
Tunega <145489020+pedro-vitor171@users.noreply.github.com>
2024-05-07 14:24:09 -0300

    Add files via upload

---
+++ Atividade 30-04-2024/pedro.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade de PHP</title>
+</head>
+<body>
+<?php
+ echo " <br>exemplo 1<br>";
+
+ $var1 = array(
+    "nome" => "Pedro Vitor",
+    "endereco" => "Rua 15 de Abril",
+    "cidade" => "Salvador",
+    "estado" => "Bahia",
+    "idade" => 17,
+    "cpf" => "111.111.111-11");
+
+ echo "O nome é " . $var1["nome"];
+ echo "<br>";
+ echo "O endereco é " . $var1["endereco"];
+ echo "<br>";
+ echo "A cidade é " . $var1["cidade"];
+ echo "<br>";
+ echo "O estado é " . $var1["estado"];
+ echo "<br>";
+ echo "A idade é " . $var1["idade"];
+ echo "<br>";
+ echo "O cpf é " . $var1["cpf"];
+ echo "<br>";
+
+ echo " <br>exemplo 2<br>";
+
+ $var2 = array(
+    "nome" => "Vinicius Morais",
+    "endereco" => "Av. Santo Amaro",
+    "cidade" => "Salvador",
+    "estado" => "Bahia",
+    "idade" => 18,
+    "cpf" => "222.222.222-22"
+ );
+ echo "O nome é " . $var2["nome"];
+ echo "<br>";
+ echo "O endereco é " . $var2["endereco"];
+ echo "<br>";
+ echo "A cidade é " . $var2["cidade"];
+ echo "<br>";
+ echo "O estado é " . $var2["estado"];
+ echo "<br>";
+ echo "A idade é " . $var2["idade"];
+ echo "<br>";
+ echo "O cpf é " . $var2["cpf"];
+ echo "<br>";
+
+ echo " <br>exemplo 3<br>";
+
+ $var3 = array(
+    "nome" => "Emilly Victoria",
+    "endereco" => "Rua 123",
+    "cidade" => "Recife",
+    "estado" => "Pernambuco",
+    "idade" => 18,
+    "cpf" => "333.333.333-33"
+ );
+ echo "O nome é " . $var3["nome"];
+ echo "<br>";
+ echo "O endereco é " . $var3["endereco"];
+ echo "<br>";
+ echo "A cidade é " . $var3["cidade"];
+ echo "<br>";
+ echo "O estado é " . $var3["estado"];
+ echo "<br>";
+ echo "A idade é " . $var3["idade"];
+ echo "<br>";
+ echo "O cpf é " . $var3["cpf"];
+ echo "<br>";
+
+ echo " <br>exemplo 4<br>";
+
+ $var4 = array(
+    "nome" => "Queila da Silva",
+    "endereco" => "Rua José de Oliveira",
+    "disciplina" => "Matemática",
+    "cidade" => "São Paulo",
+    "estado" => "São Paulo",
+    "idade" => 35,
+    "cpf" => "444.444.444-44"
+ );
+ echo "O nome é " . $var4["nome"];
+ echo "<br>";
+ echo "O endereco é " . $var4["endereco"];
+ echo "<br>";
+ echo "O disciplina é " . $var4["disciplina"];
+ echo "<br>";
+ echo "A cidade é " . $var4["cidade"];
+ echo "<br>";
+ echo "O estado é " . $var4["estado"];
+ echo "<br>";
+ echo "A idade é " . $var4["idade"];
+ echo "<br>";
+ echo "O cpf é " . $var4["cpf"];
+ echo "<br>";
+
+ echo " <br>exemplo 5<br>";
+
+ $var5 = array(
+    "nome" => "Ana Lúcia", 
+    "endereco" => "Rua Nelson Medeiros",
+    "disciplina" => "História",
+    "cidade" => "Angra dos Reis",
+    "estado" => "Rio de Janeiro",
+    "idade" => 23,
+    "cpf" => "555.555.555-55"
+ );
+ echo "O nome é " . $var5["nome"];
+ echo "<br>";
+ echo "O endereco é " . $var5["endereco"];
+ echo "<br>";
+ echo "O disciplina é " . $var5["disciplina"];
+ echo "<br>";
+ echo "A cidade é " . $var5["cidade"];
+ echo "<br>";
+ echo "O estado é " . $var5["estado"];
+ echo "<br>";
+ echo "A idade é " . $var5["idade"];
+ echo "<br>";
+ echo "O cpf é " . $var5["cpf"];
+ echo "<br>";
+
+ echo " <br>exemplo 6<br>";
+
+ $var6 = array(
+    "nome" => "Cleudson",
+    "endereco" => "Rua Miguel Bomfim",
+    "disciplina" => "Biologia",
+    "cidade" => "Belo Horizonte",
+    "estado" => "Minas Gerais",
+    "idade" => 45,
+    "cpf" => "666.666.666-66"
+ );
+ echo "O nome é " . $var6["nome"];
+ echo "<br>";
+ echo "O endereco é " . $var6["endereco"];
+ echo "<br>";
+ echo "O disciplina é " . $var6["disciplina"];
+ echo "<br>";
+ echo "A cidade é " . $var6["cidade"];
+ echo "<br>";
+ echo "O estado é " . $var6["estado"];
+ echo "<br>";
+ echo "A idade é " . $var6["idade"];
+ echo "<br>";
+ echo "O cpf é " . $var6["cpf"];
+ echo "<br>";
+
+?>
+</body>
+</html>
